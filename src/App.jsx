06d27e5e7cecd47b06c8a73baa11bda7f40c7cd9
Iman Langaran <imanlangaran@gmail.com>
2025-06-26 23:30:36 +0330

    init hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { ThemeProvider } from './context/ThemeContext'
 import NavBar from './Components/NavBar'
+import HeroSection from './Components/Sections/HeroSection'
 
 const App = () => {
   return (
     <ThemeProvider>
       <div>
         <NavBar />
+        <HeroSection />
       </div>
     </ThemeProvider>
   )
