ff71441ba9f9ea2b0527367bce48023c915a8d01
Iman Langaran <imanlangaran@gmail.com>
2025-07-04 19:21:20 +0330

    init i18n

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,23 @@ import ProjectsSection from './Components/Sections/ProjectsSection'
 import AboutSection from './Components/Sections/AboutSection'
 import ContactSection from './Components/Sections/ContactSection'
 import Footer from './Components/Sections/Footer'
+import i18n from './lang/i18n'
+import { I18nextProvider } from 'react-i18next'
 
 const App = () => {
   return (
     <ThemeProvider>
-      <div>
-        <NavBar />
-        <HeroSection />
-        <SkillsSection />
-        <ProjectsSection />
-        <AboutSection />
-        <ContactSection />
-        <Footer />
-      </div>
+      <I18nextProvider i18n={i18n}>
+        <div>
+          <NavBar />
+          <HeroSection />
+          <SkillsSection />
+          <ProjectsSection />
+          <AboutSection />
+          <ContactSection />
+          <Footer />
+        </div>
+      </I18nextProvider>
     </ThemeProvider>
   )
 }
---
+++ src/lang/i18n.jsx
@@ -0,0 +1,34 @@
+import i18n from 'i18next';
+import { I18nContext, initReactI18next } from "react-i18next";
+import en from './locales/en.json';
+import fa from './locales/fa.json';
+import {  useState } from 'react';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      En: { translation: en },
+      Fa: { translation: fa },
+    },
+    lng: "Fa",
+
+    keySeparator: false, // we do not use keys in form messages.welcome
+
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+export const useI18n = () => {
+  const [lang, setLang] = useState("Fa");
+
+  const changeLanguage = (lng) => {
+    setLang(lng);
+    i18n.changeLanguage(lng)
+  }
+
+  return { i18n, lang, changeLanguage };
+}
+
+export default i18n;
