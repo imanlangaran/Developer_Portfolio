c7a7e5c802e85a7563f766f29d776204b4aa57aa
Iman Langaran <imanlangaran@gmail.com>
2025-07-08 10:54:29 +0330

    support fa font when lang changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
-import { ThemeProvider } from './context/ThemeContext'
-import NavBar from './Components/NavBar'
-import HeroSection from './Components/Sections/HeroSection'
-import SkillsSection from './Components/Sections/SkillsSection'
-import ProjectsSection from './Components/Sections/ProjectsSection'
-import AboutSection from './Components/Sections/AboutSection'
-import ContactSection from './Components/Sections/ContactSection'
-import Footer from './Components/Sections/Footer'
-import i18n from './lang/i18n'
-import { I18nextProvider } from 'react-i18next'
+import React from "react";
+import { ThemeProvider } from "./context/ThemeContext";
+import NavBar from "./Components/NavBar";
+import HeroSection from "./Components/Sections/HeroSection";
+import SkillsSection from "./Components/Sections/SkillsSection";
+import ProjectsSection from "./Components/Sections/ProjectsSection";
+import AboutSection from "./Components/Sections/AboutSection";
+import ContactSection from "./Components/Sections/ContactSection";
+import Footer from "./Components/Sections/Footer";
+import i18n from "./lang/i18n";
+import { I18nextProvider, useTranslation } from "react-i18next";
 
-const App = () => {
+const AppContainer = () => {
+  const { i18n: i18nInstance } = useTranslation();
+  const lang = i18nInstance.language;
+
   return (
-    <ThemeProvider>
-      <I18nextProvider i18n={i18n}>
-        <div>
-          <NavBar />
-          <HeroSection />
-          <SkillsSection />
-          <ProjectsSection />
-          <AboutSection />
-          <ContactSection />
-          <Footer />
-        </div>
-      </I18nextProvider>
-    </ThemeProvider>
-  )
-}
+    <I18nextProvider i18n={i18n}>
+      <div className={`${lang === 'En' ? 'font-en' : 'font-fa'}`}>
+        <NavBar />
+        <HeroSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <AboutSection />
+        <ContactSection />
+        <Footer />
+      </div>
+    </I18nextProvider>
+  );
+};
 
-export default App
+const App = () => (
+  <ThemeProvider>
+    <AppContainer />
+  </ThemeProvider>
+);
+
+export default App;
