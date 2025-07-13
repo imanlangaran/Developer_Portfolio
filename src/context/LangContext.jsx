b395ead219bdcb66b2e384692c46f1bce4e94395
Iman Langaran <imanlangaran@gmail.com>
2025-07-13 15:28:42 +0330

    some fixes

--- src/context/LangContext.jsx
+++ src/context/LangContext.jsx
@@ -1,42 +1,46 @@
-import { createContext, useContext, useEffect, useState } from 'react'
-import { I18nextProvider } from 'react-i18next'
-import i18n from '../lang/i18n'
-import { getChangeLangDuration, getLangFromLocalStorageOrDefault, setLangToLocalStorage } from '../utils/helper';
+import { createContext, useContext, useEffect, useState } from "react";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../lang/i18n";
+import {
+  getChangeLangDuration,
+  getLangFromLocalStorageOrDefault,
+  setLangToLocalStorage,
+} from "../utils/helper";
 
 const langContext = createContext();
 
 const langDirMap = {
   En: "ltr",
-  Fa: "rtl"
+  Fa: "rtl",
 };
 
 export const LangProvider = ({ children }) => {
-  const [lang, setLang] = useState(
-    getLangFromLocalStorageOrDefault()
-  )
+  const [lang, setLang] = useState(getLangFromLocalStorageOrDefault());
 
   useEffect(() => {
-    setLang(lang);
+    document.documentElement.dir = langDirMap[lang] || "ltr";
+  }, []);
+
+  useEffect(() => {
+    // setLang(lang);
     setTimeout(() => {
       i18n.changeLanguage(lang);
-      document.documentElement.dir = langDirMap[lang] || 'ltr';
+      document.documentElement.dir = langDirMap[lang] || "ltr";
       document.documentElement.lang = lang.toLowerCase();
+      setLangToLocalStorage(lang);
     }, getChangeLangDuration("ms"));
     // localStorage.setItem('lang', lang)
-    setLangToLocalStorage(lang)
-  }, [lang])
+  }, [lang]);
 
   return (
-    <langContext.Provider
-      value={{ lang, setLang }}
-    >
+    <langContext.Provider value={{ lang, setLang }}>
       <I18nextProvider i18n={i18n}>
-        <div className={`${lang === "En" ? "font-en" : "font-fa"}`}>
+        <div className={`${i18n.language === "En" ? "font-en" : "font-fa"}`}>
           {children}
         </div>
       </I18nextProvider>
     </langContext.Provider>
-  )
-}
+  );
+};
 
 export const useLang = () => useContext(langContext);
