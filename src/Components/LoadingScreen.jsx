b0b6c8b37a0f7e9e6fdbfb19e6dca4ed99e94870
Iman Langaran <73127950+imanlangaran@users.noreply.github.com>
2025-07-14 13:44:09 +0330

    del writing in loading page

--- src/Components/LoadingScreen.jsx
+++ src/Components/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LoadingScreen = ({ percent }) => {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-black text-white z-50">
-      <div className="text-2xl mb-4">Loading assets...</div>
+      {/* <div className="text-2xl mb-4">Loading assets...</div> */}
       <div className="text-4xl font-bold">{percent}%</div>
       <div className="w-64 h-2 bg-gray-700 rounded mt-6">
         <div
