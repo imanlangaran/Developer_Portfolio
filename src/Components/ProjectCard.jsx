3a2e3e47b3e1a603f227950c28685963c54ba1ab
Iman Langaran <imanlangaran@gmail.com>
2025-07-23 19:49:46 +0330

    fix link for projects

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -64,7 +64,8 @@ const ProjectCard = ({ project, index, isDarkMode }) => {
           >
             <motion.a
               href={project.liveUrl}
-              initial={{ y: 20, opacity: 0.5 }}
+              target='_blank'
+              initial={{ y: 5, opacity: 0.5 }}
               whileHover={{ y: 0, opacity: 1, scale: 1.05 }}
               transition={{ duration: 0.3, delay: 0.1 }}
               className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full flex items-center space-x-2 text-sm font-medium transition-colors'
@@ -75,7 +76,8 @@ const ProjectCard = ({ project, index, isDarkMode }) => {
 
             <motion.a
               href={project.githubUrl}
-              initial={{ y: 20, opacity: 0.5 }}
+              target='_blank'
+              initial={{ y: 5, opacity: 0.5 }}
               whileHover={{ y: 0, opacity: 1, scale: 1.05 }}
               transition={{ duration: 0.3, delay: 0.2 }}
               className={`border-2 border-white text-white hover:bg-white hover:text-gray-900 px-4 py-2 rounded-full flex items-center space-x-2 text-sm font-medium transition-all`}
